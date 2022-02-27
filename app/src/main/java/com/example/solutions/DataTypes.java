2ad318a7ec1b07ada2503cbdb2f8fe9e11e5ca4f
isilay-subasi <isil.subasi19@gmail.com>
2022-02-27 20:42:03 +0300

    data types

---
+++ app/src/main/java/com/example/solutions/DataTypes.java
@@ -0,0 +1,17 @@
+package com.example.solutions;
+
+import java.io.*;
+import java.util.*;
+
+public class DataTypes {
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println(
+                (Integer.parseInt(scan.nextLine()) + 4) + "\n" +
+                        (Double.parseDouble(scan.nextLine()) + 4.0) + "\n" +
+                        "HackerRank ".concat(scan.nextLine()));
+
+
+    }
+}
